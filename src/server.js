d97ab0da84d16a5cf5ae9abc60de9cc12a515396
vrDevelopers615 <vrgamers615@gmail.com>
2024-08-17 23:15:02 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
+
 
 const app = express();
-app.use(bodyParser.json());
-const SECRET_KEY = "your_secret_key";
-const users = {
-  user1: "password123",
-  user2: "mypassword",
-};
+
 const PORT = 3000;
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (users[username] && users[username] === password) {
-    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
-    res.json({ token });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
-  }
-});
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
